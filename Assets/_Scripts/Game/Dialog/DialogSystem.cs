f8bc8313f7d8e6d26f07809d56f44c99dda445e4
H <h-baer@web.de>
2024-01-27 21:16:24 +0100

    Safety commit

--- Assets/_Scripts/Game/Dialog/DialogSystem.cs
+++ Assets/_Scripts/Game/Dialog/DialogSystem.cs
@@ -6,9 +6,6 @@ using Utility.Singletons;
 
 public class DialogSystem : SingletonMonoBehaviour<DialogSystem>
 {
-
-    int interactionID = 1;  //specify with who the player interacts (objects, persons, ...)
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +13,12 @@ public class DialogSystem : SingletonMonoBehaviour<DialogSystem>
     }
 
     // Update is called once per frame
-    void Update()
+    public void getDialogText(int interactionID)
     {
         if (interactionID >= 0)
         {
             string message = ConfigSingletonInstaller.Instance.DialogConfig.GetNextDialog(interactionID);
             Debug.LogError(message);
-            if (ConfigSingletonInstaller.Instance.DialogConfig.CheckIfDialogEnds(interactionID))
-            {
-                interactionID = -1;
-            }
         }
     }
 }
