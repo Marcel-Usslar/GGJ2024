5ba9fc98d7903126b92f918ccbc258e7e8656a82
H <h-baer@web.de>
2024-01-27 15:41:25 +0100

    Added LevelTimer and LevelTimerConfig

---
+++ Assets/_Scripts/Game/LevelTimer/LevelTimerConfig.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTimerConfig : ScriptableObject
+{
+    public float realLifeTimeUntilEndOfLevel_secs;
+    public float inGameStartTime;
+    public float inGameEndTime;
+}
--- Assets/_Scripts/Game/Utility/ConfigSingletonInstaller.cs
+++ Assets/_Scripts/Game/Utility/ConfigSingletonInstaller.cs
@@ -7,7 +7,9 @@ namespace Game.Utility
     public class ConfigSingletonInstaller : SingletonMonoBehaviour<ConfigSingletonInstaller>
     {
         [SerializeField] private SceneConfig _sceneConfig;
+        [SerializeField] private LevelTimerConfig _levelTimerConfig;
 
         public ISceneConfig SceneConfig => _sceneConfig;
+        public LevelTimerConfig LevelTimerConfig => _levelTimerConfig;
     }
 }
